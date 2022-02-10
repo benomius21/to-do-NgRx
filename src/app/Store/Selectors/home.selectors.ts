f10c566f10e4eb3cc35ba2d968e0d1be7b10bbb7
benomius21 <benomius@hotmail.com>
2022-02-10 16:38:01 +0100

    changes

--- src/app/Store/Selectors/home.selectors.ts
+++ src/app/Store/Selectors/home.selectors.ts
@@ -1,12 +1,10 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 import {
-  UsersPartialState,
   UserState,
 } from '../Reducers/home.reducer';
 
 export const getUserState = createFeatureSelector<
-  UsersPartialState,
   UserState
 >('users');
 
